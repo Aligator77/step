dc74d28c655ec8eccd2abf70e241000e944713da
Олег <aligator77@Aligator-Apple.local>
2017-10-14 14:58:01 +0300

    update with new model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.CharField(max_length=500)
+    likes = models.IntegerField()
+    def QuestionManager(self):
+        return self
+    def new(self):
+        return self.0 #return last added quest
+    def popular(self):
+        m = self.0
+        for s in self:
+            if(m.rating<s.rating):
+                m = s
+        return self #return sorting by rating quest
+    class Meta:
+        ordering = ['-added_at']
+
+
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    author = models.CharField(max_length=500)
+    question = models.TextField()
+
--- init.sh
+++ init.sh
@@ -7,3 +7,18 @@ sudo rm -r /etc/gunicorn.d/*
 #sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf   /etc/gunicorn.d/gunicorn_ask.conf
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
+echo 'if need create table press 1'
+read create_db
+if [ $create_db -eq 1] ; then
+	#sudo rm -r /etc/mysql/my.cnf
+	#sudo ln -sf /home/box/web/etc/my.cnf  /etc/mysql/my.cnf
+	sudo /etc/init.d/mysql start
+
+	mysql -uroot -e "CREATE DATABASE qa"
+	mysql -uroot -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY 'qapass';
+				 GRANT ALL ON qa.* TO 'qauser'@'localhost';"
+
+	#sudo python /home/box/web/ask/manage.py syncdb
+fi
