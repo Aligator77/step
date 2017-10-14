2bbd6392ff44034b9277fc1201733e6882799585
Aligator77 <alf@alf>
2017-10-14 12:51:28 +0000

    finish 2.3

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
@@ -74,14 +75,17 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qa',
+        'USER': 'qauser',
+        'PASSWORD': 'qapass',
     }
 }
-
-
-# Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,32 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
-    rating = models.IntegerField()
-    author = models.CharField(max_length=500)
-    likes = models.IntegerField()
-    def QuestionManager(self):
-        return self
-    def new(self):
-        return self.0 #return last added quest
-    def popular(self):
-        m = self.0
-        for s in self:
-            if(m.rating<s.rating):
-                m = s
-        return self #return sorting by rating quest
-    class Meta:
-        ordering = ['-added_at']
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name='+', default=1)
+    likes = models.ManyToManyField(User, default=1)
+#    def new(self):
+#        return self.0 #return last added quest
+#    def popular(self):
+#        m = self.0
+#        for s in self:
+#            if(m.rating<s.rating):
+#                m = s
+#        return self #return sorting by rating quest
+#    class Meta:
+#        ordering = ['-added_at']
 
 
 
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
-    author = models.CharField(max_length=500)
-    question = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, null=False, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, related_name='+', default=1)
+
 
